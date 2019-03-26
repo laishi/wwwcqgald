edcbb79bbe5441d82dd8744dafdc73d48796c3fb
laishi <laishi@localhost.localdomain>
2019-03-26 20:08:06 +0800

    svg header

--- cqgald/js/conf.js
+++ cqgald/js/conf.js
@@ -16,10 +16,16 @@ $(function () {
     var pageTitle
     var curentPageTitle
 
-    $.scrollify({
-        section: ".page",
-        //sectionName:false,
-        // scrollSpeed: 1100,
+$.scrollify({
+		section : ".page",
+		sectionName : "section-name",
+		easing: "easeOutExpo",
+		scrollSpeed: 1100,
+		offset : 0,
+		scrollbars: true,
+
+		afterResize:function() {},
+
 
 
 
